ae0126f8e9c519780e4ca51a0878b3989d6fa26a
hilal_dinc <hilaldincc94@gmail.com>
2025-06-05 02:13:42 +0300

    vite: gallery and form components added

---
+++ src/js/02-form.js
@@ -0,0 +1,38 @@
+import "../css/02-form.css";
+
+const form = document.querySelector(".feedback-form");
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+const STORAGE_KEY = "feedback-form-state";
+
+form.addEventListener("input", () => {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedData = localStorage.getItem(STORAGE_KEY);
+  if (savedData) {
+    const { email, message } = JSON.parse(savedData);
+    emailInput.value = email || "";
+    messageInput.value = message || "";
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const submittedData = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  console.log("Form Gönderildi:", submittedData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+});
